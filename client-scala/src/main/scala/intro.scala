c180d78808512dd5505a52ea41bf742c0422fd73
Sean Gallagher <stgallag@gmail.com>
2013-09-12 16:52:31 -0400

    writing cache: not done

--- client-scala/src/main/scala/intro.scala
+++ client-scala/src/main/scala/intro.scala
@@ -1,65 +1,25 @@
+package bridges
 import dispatch._
 import Defaults._
 import net.liftweb.json
 //import java.nio.file._
 import scalax.file.Path
 
-/** Abstract base of student-implemented structures except Nodes
-  *
-  * This would be a trait, but it needs to be inherited in Java
-  */
-abstract class StudentStructure[T] {
-    // Students need to subclass this
-    implicit val formats = json.Serialization.formats(json.NoTypeHints)
-    def push(element: T)
-    def serialize() : String
-}
-
-/** Abstract base of student-implemented structures except Nodes
-  *
-  * This would be a trait, but it needs to be inherited in Java
-  */
-case class StackNode[T](next: StackNode[T], element: T)
-
-abstract class StudentStack[T] extends StudentStructure[T] {
-    var root : StackNode[T]
-    def serialize()= {
-        val out = collection.mutable.ListBuffer[T]()
-        var node = root
-        while(node != null) {
-            node = node.next
-            out += node.element
-        }
-        json.Serialization.write(out.result)
-    }
-}
-
-/** Pointer-based reference example of a student stack */
-class ReferenceStack[T] extends StudentStack[T] {
-    // TODO: Replace this with Java
-    var root: StackNode[T] = null
-    
-    def push(element: T) {
-        root = new StackNode[T](root, element)
-    }
-    
-    def pop()= {
-        if (root == null)
-            null
-        else {
-            val e = root.element
-            root = root.next
-            e
-        }
-    }
-}
 
 /** Session object capable of being loaded from a JSON file */
-case class Session(url: String, refreshed: Date, cache: Map[String, json.JObject]) {
-    def entries= {
+case class Session(url: String, cache: Map[String, json.JObject]) {
+    def entries(stream: String)= {
+        // TODO: expire
+        if (cache.contains(stream))
+            cache(stream)
+        else
+            // TODO: make mutable
+            cache(stream) = live(stream)
+            
     }
     def live(stream: String) : List[Any]= {
-        val location = url(s"http://localhost/$stream.json")
+        
+        val location = dispatch.url(s"http://localhost/$stream.json")
         val request = Http.configure(_ setFollowRedirects true)(location OK as.String)
         json.parse(request()) match {
             case json.JArray(entries) => entries
@@ -73,16 +33,25 @@ object Session {
     implicit val formats = json.Serialization.formats(json.NoTypeHints)
     val config_path = Path.fromString(System.getProperty("user.home")) / ".config" / "bridges"
     
+    def new_session()= {
+            /* 
+             *  Initiate a session
+             *  The API here isn't known yet; STUB, TODO
+             */
+        Session("Example", Map[String, json.JObject]())
+    }
+    
     def load()= {
         // TODO: Windows + Mac configuration locations
         if (config_path.exists) {
-            json.Serialization.read[Session](config_path.chars().mkString)
+            try {
+                json.Serialization.read[Session](config_path.chars().mkString)
+            } catch {
+                case mape: json.MappingException => new_session()
+            }
         } else {
             config_path.createFile()
-            /* 
-             *  Initiate a session
-             *  The API here isn't known yet.
-             */
+            new_session()
         }
     }
     
@@ -91,7 +60,7 @@ object Session {
         // Request a new session
         val session_init_url = url("http://localhost/session")
         // This API is not determined yet
-        val request = Http(location.POST)
+        val request = Http(session_init_url.POST)
         
     }
     
@@ -101,13 +70,10 @@ object Session {
 }
 
 class Retrieve(stream: String, structure: StudentStructure[Any]) {
-
-    // TODO: Create a session, stream from and to the server using it
-    // TODO: Cache
-    val session: Session
+    val session = Session.load()
     
     def getMultiple() {
-        for (entry <- live(stream)) structure.push(entry)
+        for (entry <- session.live(stream)) structure.push(entry)
     }
 }
 
