abc2b397460fa9e8496e21f892d230c3f6be62ae
Sean Gallagher <stgallag@gmail.com>
2013-10-03 08:00:34 -0400

    Reworked inheritance to improve testability

--- client-scala/src/test/scala/Session.scala
+++ client-scala/src/test/scala/Session.scala
@@ -3,7 +3,13 @@ import bridges.Session
 
 class SessionTest extends FlatSpec with Matchers {
     "Session" should "be able to initialize without a cache" in {
-        var s = new Session("username", "password", assignment=0)
+        val s = new Session("", "", assignment=0)
         s.cache should be (Map())
+    }
+    
+    "Session" should "fail gracefully for status uploads" in {
+        val s = new Session("", "", assignment=0)
+        // Should not throw
+        s.send_state("sample serial")
     }
 }
