c285b8c71bc2e86815b53211da2e5d69db7374a4
Sean Gallagher <stgallag@gmail.com>
2013-12-22 13:29:27 -0500

    Installed updated rails

---
+++ server-ruby/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ServerRuby::Application.config.secret_key_base = 'bc650979c1d0240607f43cc4c4382ca34cbf629575f2c26703bf8adb9c6f7cf020f28f390b79cc71e0d35e2769d3c259536fc3e7a7fa52407bd5f86a78d674ca'
---
+++ server-ruby/spec/views/welcome/home.html.erb_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe "welcome/home.html.erb" do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
