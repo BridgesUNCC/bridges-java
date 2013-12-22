1c342bfa3879717d773cbbd67fd597c627387c3e
Sean Gallagher <stgallag@gmail.com>
2013-12-22 13:04:23 -0500

    Rails was too old. regenerate

--- server-ruby/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-ServerRuby::Application.config.secret_token = '9d46dcd7675112a4c0ae1a9169a14e333d65094c2b251ce17315c3e7285b46509475f6514bc2eb358fc88f98bbe283da34cbde3a98785b147931c91fdddf33f0'
