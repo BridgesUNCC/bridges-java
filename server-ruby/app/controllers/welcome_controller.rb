5fa890ade2344f9ffef341e5469fb3b9076a4a3b
Sean Gallagher <stgallag@gmail.com>
2013-12-26 11:25:51 -0500

    Fix redirects

--- server-ruby/app/controllers/welcome_controller.rb
+++ server-ruby/app/controllers/welcome_controller.rb
@@ -1,7 +1,13 @@
 class WelcomeController < ApplicationController
   def home
+    if current_user
+      redirect_to dashboard_path
+    end
   end
   
   def dashboard
+    if not current_user
+      redirect_to root_path
+    end
   end
 end
