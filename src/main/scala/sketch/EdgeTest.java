70d115c888069aded6955fef7385d8b798dab454
Michael Vitulli <mvitulli@uncc.edu>
2014-05-29 13:54:23 -0400

    Started Edge testing

---
+++ src/main/scala/sketch/EdgeTest.java
@@ -0,0 +1,49 @@
+package sketch;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class EdgeTest {
+
+	@Test
+	public final void testGetColor() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testSetColor() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testGetDash() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testSetDash() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testGetThickness() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testSetThickness() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testGetOpacity() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testSetOpacity() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
