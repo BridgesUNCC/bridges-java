54399b5f8df55f5c783a18d346e132affda5536d
Mihai Mehedint <mmehedin@uncc.edu>
2014-09-11 09:52:16 -0400

    Minor driver update

--- src/main/java/edu/uncc/cs/bridgesdrivers/QueueDriver.java
+++ src/main/java/edu/uncc/cs/bridgesdrivers/QueueDriver.java
@@ -6,21 +6,24 @@ public class QueueDriver {
 
 	public static void main(String[] args) {
 		Queue<Follower> queue = new Queue<Follower>();
-		Bridge.init(3, "1157177351793", queue, "mmehedin@uncc.edu");
+		Bridge.init(6, "1157177351793", queue, "mmehedin@uncc.edu");
+		
 		//This works also:
 		//AbstractVertex<Follower> entity1= new QueueNode<>(new Follower("entity1"), queue);
 		//AbstractVertex<Follower> entity2= new QueueNode<>(new Follower("entity2"), queue);
+		
+		//Adding elements to the queue
 		queue.enQueue(new Follower("entity1"));
 		queue.enQueue(new Follower("entity2"));
 		
+		//the queue is emptied
 		queue.clear();
-		
 		
 		
 		queue.enQueue(new Follower("entity2"));
 		queue.enQueue(new Follower("entity3"));
 		queue.enQueue(new Follower("entity4"));
-		System.out.println(queue.vertices);
+		//System.out.println(queue.vertices);
 		
 		//after this statement the elements are not connected
 		//the user can display the queue with or without links(edges) in between
@@ -32,15 +35,12 @@ public class QueueDriver {
 		queue.enQueue(new Follower("entity8"));
 		queue.enQueue(new Follower("entity9"));
 		queue.enQueue(new Follower("entity10"));
-		System.out.println(queue.vertices);
 		
 		//create a circular Queue based on LList
-		queue.circularLList();
-		queue.deQueue();
-		System.out.println(queue.vertices);
+		//queue.circularLList();
 		queue.deQueue();
 		queue.deQueue();
-		queue.enQueue(new Follower("entity11"));
+		queue.deQueue();
 		//queue.deQueue();
 		//queue.deQueue();
 	
