24f063f44129e1df05be31b40e12e53d0d130e7f
Mihai Mehedint <mmehedin@uncc.edu>
2014-10-27 15:27:07 -0400

    corrected the original tree driver

--- src/main/java/edu/uncc/cs/bridgesdrivers/TreeDriver.java
+++ src/main/java/edu/uncc/cs/bridgesdrivers/TreeDriver.java
@@ -6,38 +6,38 @@ public class TreeDriver {
 
 	public static void main(String[] args) throws Exception {
 
-		TreeVisualizer tree = new TreeVisualizer();//Creating tree structure
+		TreeVisualizer<Actor> tree = new TreeVisualizer<>();//Creating tree structure
 		
 		Bridge.init(23, "300587042698", tree, "mmehedin@uncc.edu");//Heroku
 		//Bridge.update(); //this doesn't work, the tree is empty
 				
-		tree.insert(new BSTNode("Root, 12", 12));
+		tree.insert(new BSTNode<>(new Actor("Root, 12"), 12));
 		System.out.println("Root: " + tree.getRoot().getIdentifier());
 		
-		tree.insert(new BSTNode("Steve, 15", 15));		
+		tree.insert(new BSTNode<>(new Actor("Steve, 15"), 15));		
 		System.out.println("Right Child: " + tree.getRoot().getRightChild().getIdentifier());		
 		
-		tree.insert(new BSTNode("D, 18", 18));
+		tree.insert(new BSTNode<>(new Actor("D, 18"), 18));
 		
-		tree.insert(new BSTNode("John, 10", 10));
+		tree.insert(new BSTNode<>(new Actor("John, 10"), 10));
 		//System.out.println("Left Child: " + tree.getRoot().getLeftChild().getIdentifier());
 		
 		Bridge.update();
 		
-		tree.insert(new BSTNode("Frank, 11", 11));		
+		tree.insert(new BSTNode<>(new Actor("Frank, 11"), 11));		
 		//System.out.println("Left-Right Child: " + tree.getRoot().getLeftChild().getRightChild().getIdentifier());
 		
-		tree.insert(new BSTNode("Dave, 8", 8));
+		tree.insert(new BSTNode<>(new Actor("Dave, 8"), 8));
 		
-		tree.insert(new BSTNode("C, 13", 13));
+		tree.insert(new BSTNode<>(new Actor("C, 13"), 13));
 		
-		tree.insert(new BSTNode("Chris, 5", 5));
+		tree.insert(new BSTNode<>(new Actor("Chris, 5"), 5));
 		
-		tree.insert(new BSTNode("Ada, 1", 1));
+		tree.insert(new BSTNode<>(new Actor("Ada, 1"), 1));
 
-		tree.insert(new BSTNode("Betsy, 7", 7));
+		tree.insert(new BSTNode<>(new Actor("Betsy, 7"), 7));
 		
-		tree.insert(new BSTNode("B, 6", 6));
+		tree.insert(new BSTNode<>(new Actor("B, 6"), 6));
 		
 		tree.getRoot().setColor("red").setSize(50);
 		
