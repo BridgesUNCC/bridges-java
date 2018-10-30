4c7a20e2ff72db40d078fbf3bf985526bba1a0fe
squeetus <burlinsond2@gmail.com>
2018-10-30 17:16:31 -0400

    removing old commented code

--- src/main/java/bridges/base/SymbolCollection.java
+++ src/main/java/bridges/base/SymbolCollection.java
@@ -53,9 +53,7 @@ public class SymbolCollection extends DataStruct {
   /*
    *   This method examines whether the axes should be expanded to ensure all shapes are shown
    */
-  public void updateAxisDomains(Symbol s) {
-    // Float[] loc = s.getLocation();
-    // Float[] size = s.getDimensions();
+  private void updateAxisDomains(Symbol s) {
     Float[] dims = s.getDimensions();
 
     // check x axis
@@ -87,105 +85,7 @@ public class SymbolCollection extends DataStruct {
 
       symbol_json.add(symbol.getValue().getJSONRepresentation());
     }
-    // System.out.println("dimensions: " + domainX[0] + "," + domainX[1] + " -- " + domainY[0] + "," + domainY[1]);
 
-    // return "\"domainX\":[" + domainX[0] + "," + domainX[1] + "],\"domainY\":[" + domainY[0] + "," + domainY[1] + "]," + "\"symbols\":" + symbol_json + "}";
     return "\"domainX\":[" + -domain + "," + domain + "],\"domainY\":[" + -domain + "," + domain + "]," + "\"symbols\":" + symbol_json + "}";
   }
 }
-// 
-// package bridges.base;
-//
-// import bridges.base.DataStruct;
-//
-// import java.util.HashMap;
-// import java.util.Map;
-// import java.util.Map.Entry;
-//
-// import org.json.simple.JSONArray;
-//
-//
-//
-// /*
-//  * @brief the ShapeCollection represents a collection of symbols (shapes, polygons, and text) to visualize in Bridges
-//  * @author David Burlinson
-// */
-// public class SymbolCollection extends DataStruct {
-//   // keep track of the shape elements; useful
-//   // to maintain their properties
-//   private final HashMap<String, Symbol> symbols;
-//
-//   // default domain (assuming square coordinate space)
-//   //  domian emanates in x and y directions, both positive and negative, from 0,0
-//   protected Float domain = 100.0f;
-//
-//   /**
-// 	 *
-// 	 *	Constructor
-// 	 */
-//   public SymbolCollection() {
-//     symbols = new HashMap<String, Symbol>();
-//   }
-//
-//   /**
-//    *	This method gets the data structure type
-//    *
-//    *	@return  The date structure type as a string
-//    *
-//    */
-//   public String getDataStructType() {
-//     return "SymbolCollection";
-//   }
-//
-//  /**
-//   *   This method adds a symbol to the collection
-//   */
-//   public void addSymbol(Symbol s) {
-//     // note: it is the user's responsibility to handle
-//     //  duplicates where desired
-//     symbols.put(s.getIdentifier(), s);
-//   }
-//
-//   /*
-//    *   This method examines whether the axes should be expanded to ensure all shapes are shown
-//    */
-//   public void updateAxisDomains(Symbol s) {
-//     // Float[] loc = s.getLocation();
-//     // Float[] size = s.getDimensions();
-//     Float[] dims = s.getDimensions();
-//
-//     // check x axis
-//     if(Math.abs(dims[0]) > domain) {
-//       domain = Math.abs(dims[0]);
-//     }
-//     if(Math.abs(dims[1]) > domain) {
-//       domain = Math.abs(dims[1]);
-//     }
-//
-//     // check y axis
-//     if(Math.abs(dims[2]) > domain) {
-//       domain = Math.abs(dims[2]);
-//     }
-//     if(Math.abs(dims[3]) > domain) {
-//       domain = Math.abs(dims[3]);
-//     }
-//   }
-//
-//   /*
-//    *	Get the JSON representation of the the data structure
-//    */
-//   public String getDataStructureRepresentation() {
-//     JSONArray symbol_json = new JSONArray();
-//     for(Entry<String, Symbol> symbol : symbols.entrySet()) {
-//
-//       // update axis domains where appropriate for each shape
-//       updateAxisDomains(symbol.getValue());
-//
-//       symbol_json.add(symbol.getValue().getJSONRepresentation());
-//     }
-//     // System.out.println("dimensions: " + domainX[0] + "," + domainX[1] + " -- " + domainY[0] + "," + domainY[1]);
-//
-//     // return "\"domainX\":[" + domainX[0] + "," + domainX[1] + "],\"domainY\":[" + domainY[0] + "," + domainY[1] + "]," + "\"symbols\":" + symbol_json + "}";
-//     return "\"domainX\":[" + -domain + "," + domain + "],\"domainY\":[" + -domain + "," + domain + "]," + "\"symbols\":" + symbol_json + "}";
-//   }
-// }
