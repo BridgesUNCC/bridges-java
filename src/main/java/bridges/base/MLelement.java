43aa5310be6409953d9243e08f5dc016a5b85d74
Kalpathi Subramanian <kalpathi@cci0vjg1j2aws.uncc.edu>
2019-07-23 19:27:11 -0400

    ML element doc updated

--- src/main/java/bridges/base/MLelement.java
+++ src/main/java/bridges/base/MLelement.java
@@ -5,12 +5,21 @@ import java.util.Vector;
 /**
  * 	@brief This class can be used to instantiate Multi-list Elements.
 
- * 	This class extends SLelement (singly linked list element) to build multi-lists;
- *	Multilist elements contain a tag that indicates if the element is a sublist or not;
- *	If the element points to a sublist, then the sublist field is the beginning of
- *	this sublist. If not, the data field contains the user specified data item and
- *	list continues (getNext()/setNext()). As in singly linked elements, the next pointer
- *	points to the following list element of the list or sublist.
+ * This class extends SLelement (singly linked list element) to build multi-lists;
+ * Multilist elements contain a tag (boolean) that indicates if the element 
+ * contains a sublist or not; if the tag is true, then there is a sublist beginning
+ * at this node and the starting point is the `sublist' field in the element. 
+ * If the tag is false, then the list continues as a normal singly linked list. 
+ * The sublists are re recursive: any sublist can have its own sublists and so on.
+ *
+ * As in singly linked elements, the next pointer points to the following list element and 
+ * each element contains a generic application specific object.
+ * This class extends SLelement (singly linked list element) to build multi-lists;
+ * Multilist elements contain a tag that indicates if the element is a sublist or not;
+ * If the element points to a sublist, then the sublist field is the beginning of
+ * this sublist. If not, the data field contains the user specified data item and
+ * list continues (getNext()/setNext()). As in singly linked elements, the next pointer
+ * points to the following list element of the list or sublist.
  *
  * 	Multi-list elements contain a visualizer (ElementVisualizer) object for setting
  *	visual attributes (color, shape, opacity, size), necessary for displaying
