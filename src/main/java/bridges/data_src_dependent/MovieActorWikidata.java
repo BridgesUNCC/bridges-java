97b4fe1270192f4ba123c7873bc769fecbf598ce
Alec Goncharow <Alec.Goncharow@gmail.com>
2019-08-09 18:58:27 -0400

    add MovieActorWikidata class

---
+++ src/main/java/bridges/data_src_dependent/MovieActorWikidata.java
@@ -0,0 +1,47 @@
+package bridges.data_src_dependent;
+
+public class MovieActorWikidata extends DataSource {
+    private String movieURI,
+        actorURI,
+        movieName,
+        actorName;
+
+    public MovieActorWikidata(String movieURI, String actorURI, String movieName, String actorName) {
+        this.movieURI = movieURI;
+        this.actorURI = actorURI;
+        this.movieName = movieName;
+        this.actorName = actorName;
+    }
+
+    public String getMovieURI() {
+        return movieURI;
+    }
+
+    public void setMovieURI(String movieURI) {
+        this.movieURI = movieURI;
+    }
+
+    public String getActorURI() {
+        return actorURI;
+    }
+
+    public void setActorURI(String actorURI) {
+        this.actorURI = actorURI;
+    }
+
+    public String getMovieName() {
+        return movieName;
+    }
+
+    public void setMovieName(String movieName) {
+        this.movieName = movieName;
+    }
+
+    public String getActorName() {
+        return actorName;
+    }
+
+    public void setActorName(String actorName) {
+        this.actorName = actorName;
+    }
+}
