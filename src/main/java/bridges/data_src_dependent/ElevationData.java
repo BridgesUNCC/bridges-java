6c8ee5e0805c99715ee78d0588e8a622313eab15
Kalpathi Subramanian <krs@uncc.edu>
2020-04-16 11:18:22 -0400

    elevation data documented

--- src/main/java/bridges/data_src_dependent/ElevationData.java
+++ src/main/java/bridges/data_src_dependent/ElevationData.java
@@ -1,16 +1,37 @@
 package bridges.data_src_dependent;
 
+/**
+ * Object that holds elevation data retrieved from NOAA repository
+
+ * @author Jay Strahler
+ *
+ */
+
 public class ElevationData {
 
+	// elevation data
     private int [][] data;
+
+	// data dimensions
     private int cols;
     private int rows;
+
+	// origin
     private double xll;
     private double yll;
+
+	// cell size
     private double cellsize;
+
+	// max val in dataset
     private int maxVal;
 
 
+	/** 
+	 *
+	 * constructors
+	 *
+	 */
     public ElevationData() {
         this.data = null;
         this.cols = 0;
@@ -31,61 +52,129 @@ public class ElevationData {
         this.setMaxVal(maxVal);
     }
 
+	/** 
+	 *
+	 * get elev. data
+	 *
+	 */
     public int[][] getData(){
         return data;
     }
 
+	/** 
+	 *
+	 * set elev. data
+	 *
+	 */
     public void setData(int[][]data){
         this.data = data;
     }
 
+	/** 
+	 *
+	 * get width
+	 *
+	 */
     public int getCols(){
         return cols;
     }
 
+	/** 
+	 *
+	 * set width
+	 *
+	 */
     public void setCols(int cols){
         this.cols = cols;
     }
 
+	/** 
+	 *
+	 * get height
+	 *
+	 */
     public int getRows(){
         return rows;
     }
 
+	/** 
+	 *
+	 * set height
+	 *
+	 */
     public void setRows(int rows){
         this.rows = rows;
     }
 
+	/** 
+	 *
+	 * get origin X
+	 *
+	 */
     public double getxll(){
         return xll;
     }
 
+	/** 
+	 *
+	 * set origin X
+	 *
+	 */
     public void setxll(double xll){
         this.xll = xll;
     }
 
+	/** 
+	 *
+	 * get origin Y
+	 *
+	 */
     public double getyll(){
         return yll;
     }
     
+	/** 
+	 *
+	 * set origin X
+	 *
+	 */
     public void setyll(double yll){
         this.yll = yll;
     }
 
+	/** 
+	 *
+	 * get cell size
+	 *
+	 */
     public double getCellSize(){
         return cellsize;
     }
 
+	/** 
+	 *
+	 * set cell size
+	 *
+	 */
     public void setCellSize(double cellsize){
         this.cellsize = cellsize;
     }
 
+	/** 
+	 *
+	 * get max value
+	 *
+	 */
     public int getMaxVal(){
         return maxVal;
     }
 
+	/** 
+	 *
+	 * set max value
+	 *
+	 */
     public void setMaxVal(int maxVal){
         this.maxVal = maxVal;
     }
-
-
 }
