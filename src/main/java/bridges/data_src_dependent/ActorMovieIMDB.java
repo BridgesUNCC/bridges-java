3626353e132d0429b3e1e4685283cb57d685487e
Kalpathi Subramanian <krs@uncc.edu>
2020-12-26 17:54:10 -0500

    doc updates

--- src/main/java/bridges/data_src_dependent/ActorMovieIMDB.java
+++ src/main/java/bridges/data_src_dependent/ActorMovieIMDB.java
@@ -2,63 +2,109 @@ package bridges.data_src_dependent;
 
 import java.util.Vector;
 
+/**
+ * @brief This class represents an actor-movie pair object, to be used with the 
+ * IMDB actor-movie dataset
+ *
+ * @author Kalpathi Subramanian
+ */
 public class ActorMovieIMDB extends DataSource {
 
-	/**
-	 * The constructor
-	 */
 	private String actor, movie;
 	private double rating;
 	private Vector<String> genres;
 
+	/**
+	 * Constructor
+	 */
 	public ActorMovieIMDB () {
 		super.setLabel("");
 	}
 
+	/**
+	 * Constructor
+	 * @param act_movie label for dataset
+	 */
+	public ActorMovieIMDB () {
+	}
 	public ActorMovieIMDB(String act_movie) {
 		super.setLabel(act_movie);
 	}
 
 	/**
-	 * This method returns the string name
+	 * This method returns the dataset label
+	 * @return the dataset label
 	 */
 	public String getName() {
 		return super.getLabel();
 	}
 
 	/**
-	 * This method sets the string name
+	 * This method sets the data set label
+	 * @param name  label to be added to dataset
 	 */
 	public void setName(String name) {
 		super.setLabel(name);
 	}
 
+	/**
+	 * get the actor from this object
+	 * @return actor (string) 
+	 */
 	public String getActor() {
 		return actor;
 	}
+	/**
+	 * set the actor from this object
+	 * @param actor actor (string) to be set
+	 */
 	public void setActor (String a) {
 		actor = a;
 	}
 
+	/**
+	 * get the movie from this object
+	 * @return movie (string) 
+	 */
 	public String getMovie() {
 		return movie;
 	}
+	/**
+	 * set the movie from this object
+	 * @param m movie (string) to be set
+	 */
 	public void setMovie (String m) {
 		movie = m;
 	}
 
+	/**
+	 * get the movie rating from this object
+	 * @return rating (double) 
+	 */
 	public double getMovieRating() {
 		return rating;
 	}
 
+	/**
+	 * set the movie rating for this object
+	 * @param r rating (double) to be set
+	 */
 	public void setMovieRating(double r) {
 		this.rating = r;
 	}
 
+	/**
+	 * get the genres of this movie
+	 * @return genres  (vector of strings)
+	 */
 	public Vector<String> getGenres() {
 		return genres;
 	}
 
+	/**
+	 * set the genres for this movie
+	 * @param g genres to be set for the movie (vector of strings)
+	 */
 	public void setGenres(Vector<String> g) {
 		this.genres = g;
 	}
--- src/main/java/bridges/data_src_dependent/ActorMovieWikidata.java
+++ src/main/java/bridges/data_src_dependent/ActorMovieWikidata.java
@@ -1,11 +1,23 @@
 package bridges.data_src_dependent;
 
+/** 
+ * This class is used with actor movie data retrieved from Wikidata
+ *
+ * @author Erik Saule
+ */
 public class ActorMovieWikidata extends DataSource {
 	private String movieURI,
 			actorURI,
 			movieName,
 			actorName;
 
+	/**
+	 * Constructor
+	 * @param movieURI 
+	 * @param actorURI
+	 * @param movieName name of movie (string)
+	 * @param actorName name of actor (string)
+	 */
 	public ActorMovieWikidata(String movieURI, String actorURI, String movieName, String actorName) {
 		this.movieURI = movieURI;
 		this.actorURI = actorURI;
@@ -13,34 +25,66 @@ public class ActorMovieWikidata extends DataSource {
 		this.actorName = actorName;
 	}
 
+	/**
+	 * get movie URI
+	 * @return movie URI
+	 */
 	public String getMovieURI() {
 		return movieURI;
 	}
 
+	/**
+	 * set movie URI
+	 * @param movie URI to be set
+	 */
 	public void setMovieURI(String movieURI) {
 		this.movieURI = movieURI;
 	}
 
+	/**
+	 * get actor  URI
+	 * @return actor  URI
+	 */
 	public String getActorURI() {
 		return actorURI;
 	}
 
+	/**
+	 * set actor URI
+	 * @param actor URI to be set
+	 */
 	public void setActorURI(String actorURI) {
 		this.actorURI = actorURI;
 	}
 
+	/**
+	 * get movie name
+	 * @return movie name
+	 */
 	public String getMovieName() {
 		return movieName;
 	}
 
+	/**
+	 * set movie name
+	 * @param movie name to be assigned
+	 */
 	public void setMovieName(String movieName) {
 		this.movieName = movieName;
 	}
 
+	/**
+	 * get actor name
+	 * @return actor name
+	 */
 	public String getActorName() {
 		return actorName;
 	}
 
+	/**
+	 * set actor name
+	 * @param actor name to be assigned
+	 */
 	public void setActorName(String actorName) {
 		this.actorName = actorName;
 	}
