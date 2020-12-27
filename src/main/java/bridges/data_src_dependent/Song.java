370077e4043aacbaf13f3d741f4a3e7dd2304c2f
Kalpathi Subramanian <krs@uncc.edu>
2020-12-26 19:50:33 -0500

    docs update

--- src/main/java/bridges/data_src_dependent/Song.java
+++ src/main/java/bridges/data_src_dependent/Song.java
@@ -36,6 +36,15 @@ public class Song extends DataSource {
 		artist = song = album = lyrics = release_date = "";
 	}
 
+	/**
+	 *	 Song constructor
+	 *
+	 * @param artist  artist of song
+	 * @param song    song title
+	 * @param album   album title
+	 * @param lyrics  lyrics of song (string)
+	 * @param release date  date released
+	 */
 	public Song(String artist, String song, String album, String lyrics, String release_date) {
 		this.artist = artist;
 		this.song = song;
@@ -44,37 +53,77 @@ public class Song extends DataSource {
 		this.release_date = release_date;
 	}
 
+	/**
+	 *	 Get song artist
+	 *   @return artist of song
+	 */
 	public String getArtist() {
 		return artist;
 	}
+	/**
+	 *	 Set song artist
+	 *   @param artist artist to set
+	 */
 	public 	void setArtist (String artist) {
 		this.artist = artist;
 	}
 
+	/**
+	 *	 Get song title
+	 *   @return title of song (string)
+	 */
 	public 	String getSongTitle() {
 		return song;
 	}
+	/**
+	 *	 Set song title
+	 *   @param title song title  to set
+	 */
 	public 	void setSongTitle(String song) {
 		this.song = song;
 	}
 
+	/**
+	 *	 Get album title containing the song
+	 *   @return album title  (string)
+	 */
 	public 	String getAlbumTitle() {
 		return album;
 	}
+	/**
+	 *	 Set song album
+	 *   @param album song album  to set
+	 */
 	public 	void setAlbumTitle(String album) {
 		this.album = album;
 	}
 
+	/**
+	 *	 Get lyrics of the song
+	 *   @return lyrics of  song (string)
+	 */
 	public 	String getLyrics() {
 		return lyrics;
 	}
+	/**
+	 *	 Set song lyrics
+	 *   @param lyrics of song to set
+	 */
 	public 	void setLyrics(String lyrics) {
 		this.lyrics = lyrics;
 	}
 
+	/**
+	 *	 Get release date  of the song
+	 *   @return release date of  song (string)
+	 */
 	public String getReleaseDate() {
 		return release_date;
 	}
+	/**
+	 *	 Set release date of song 
+	 *   @param release_date date of release to set
+	 */
 	public void setReleaseDate(String release_date) {
 		this.release_date = release_date;
 	}
