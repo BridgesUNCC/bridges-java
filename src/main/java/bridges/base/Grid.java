2f7ecc2359a8dd83372d522e495200e7fd77d64d
Erik Saule <esaule@uncc.edu>
2022-05-24 21:44:06 -0400

    resize grids and colorgrids

--- src/main/java/bridges/base/Grid.java
+++ src/main/java/bridges/base/Grid.java
@@ -95,17 +95,23 @@ public class Grid<E> extends DataStruct {
 	if (size[1] < oldsize[1]) {
 	    //shrink every row
 	    for (int i=0; i<oldsize[0]; i++) {
-		grid.get(i).removeRange(size[1], oldsize[1]);
+		//really grid.get(i).removeRange(size[1], oldsize[1]); but it is protected
+		for (int j=size[1]; j<oldsize[1]; ++j)
+		    grid.get(i).remove(size[1]);
 	    }
 	} else if (oldsize[1]< size[1]) {
-	    //add some nulls
-	    for (int i=oldsize[0]; i<oldsize[1]; i++) {
-		grid.get(i).add(null);
+	    //add some nulls at the end of every row
+	    for (int i=0; i<oldsize[0]; i++) {
+		for (int j=oldsize[1];j<size[1];++j) {
+		    grid.get(i).add(null);
+		}
 	    }
 	}
 	if (size[0] < oldsize[0]) {
 	    //shrink row
-	    grid.removeRange(size[0], oldsize[0]);
+	    //really grid.removeRange(size[0], oldsize[0]); but it is protected
+	    for (int j=size[0]; j<oldsize[0]; ++j)
+		grid.remove(size[0]);
 	}
 	else if (oldsize[0] < size[0]) {
 	    //add empty rows
