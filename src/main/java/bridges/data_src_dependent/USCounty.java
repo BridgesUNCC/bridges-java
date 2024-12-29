53c5719028d1d823d2d920074adc6e6d7dc19242
Kalpathi Subramanian <krs@charlotte.edu>
2024-12-29 00:35:53 -0500

    doxygen updates to County

--- src/main/java/bridges/data_src_dependent/USCounty.java
+++ src/main/java/bridges/data_src_dependent/USCounty.java
@@ -4,19 +4,22 @@ import java.lang.String;
 import bridges.base.Color;
 
 /**
-* @brief Class that holds US County information
-*
-*
-* A tutorial on how to use the these objects is available at:
-* ???
-*
-* Each County object contains its county name, state name, geoid, fips code,
-* stroke color, stroke width, fill color, stroke color
-*
-* @author Kalpathi Subramanian, Erik Saule
-*
-* @date 12/22/24
-*/
+ * @brief Class that holds US County information
+ *
+ * This object is used alongside with the USMap object. Each county
+ * has a stroke color and fill color attributes. Each county has a geoid
+ * and FIPS code for identification. The geoid is used as the key in the
+ * USState object to effic
+ *
+ * Each County object contains its county name, state name, geoid, fips code,
+ * stroke color, stroke width, fill color, stroke color
+ *
+ * A tutorial on how to use the these objects is available at:
+ * ???
+ *
+ * @author Kalpathi Subramanian, Erik Saule
+ * @date 12/22/24
+ */
 
 public class USCounty {
 
@@ -25,6 +28,9 @@ public class USCounty {
 	private float stroke_width;
 	Boolean hide_flag;			// county visibility
 	
+	/**
+	 * @brief Default constructor
+	 */
 	public USCounty() {
 		this.county_name = new String();
 		this.state_name = new String();
@@ -35,6 +41,14 @@ public class USCounty {
 		this.stroke_color = new Color(0, 0, 255);
 		this.hide_flag = false;
 	}
+	/**
+	 * @brief Constructor with county related parameters
+	 *
+	 * @param geoid  GeoId of the county
+	 * @param fips_code  FIPSa Code of the county
+	 * @param county_name  county
+	 * @param state_name   state containing county
+	 */
 	public USCounty(String geoid, String fips_code, String county_name,
 			String state_name) {
 		this.county_name = county_name;
